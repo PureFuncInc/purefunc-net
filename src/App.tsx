237f5664aa3049987b13e2db43531a752a2def72
Vincent Huang MacBook <yfr.huang@hotmail.com>
2024-01-29 22:44:02 +0800

    :art:  the structure and styling of the 'App' component. Modifications include adding new spans to the navigation, improving the responsiveness and visual arrangement of sections, and integrating a footer. The changes are in line with the transition towards using Tailwind CSS for more versatile and utility-based styling.

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export default function App() {
             className={`w-6 h-6`}
             alt="logo"></img>
         </a>
+        <span>部落格</span>
         <span>技術棧</span>
         <span>我們的服務</span>
         <span>領域</span>
@@ -20,34 +21,49 @@ export default function App() {
         <span>團隊</span>
         <button className={`py-1 px-2 border-gray-500 border-2 rounded-xl`}>Contact Us</button>
       </nav>
-      <section className={`flex flex-wrap gap-8 mx-auto justify-center`}>
-        <div className={`flex flex-col gap-2 justify-center`}>
+      <section className={`flex flex-wrap gap-8 mx-auto items-center justify-center`}>
+        <div className={`flex flex-col gap-2`}>
           <h1 className={`text-4xl font-bold`}>$_purefunc</h1>
           <span className={`flex flex-col items-end`}>
-          <p className={`text-lg font-medium`}>Software Development Company</p>
-          <p className={`text-sm`}>2021.10.26 ~</p>
-        </span>
+            <p className={`text-lg font-medium`}>Software Development Company</p>
+            <p className={`text-sm`}>2021.10.26 ~</p>
+          </span>
         </div>
         <img src={imac} className={`w-[360px]`}></img>
       </section>
-      <section className={`flex flex-col items-center`}>
-        <h2>技術棧</h2>
+      <section className={`flex flex-col mx-auto`}>
+        <h2 className={`text-lg font-medium`}>技術棧</h2>
+        <div className={`w-[360px] h-[90px] bg-gray-400`}>
+        </div>
       </section>
-      <section className={`flex flex-col items-center`}>
-        <h2>我們的服務</h2>
+      <section className={`flex flex-col mx-auto`}>
+        <h2 className={`text-lg font-medium`}>我們的服務</h2>
+        <div className={`w-[360px] h-[90px] bg-gray-400`}>
+        </div>
       </section>
-      <section className={`flex flex-col items-center`}>
-        <h2>領域</h2>
+      <section className={`flex flex-col mx-auto`}>
+        <h2 className={`text-lg font-medium`}>領域</h2>
+        <div className={`w-[360px] h-[90px] bg-gray-400`}>
+        </div>
       </section>
-      <section className={`flex flex-col items-center`}>
-        <h2>FAQ</h2>
+      <section className={`flex flex-col mx-auto`}>
+        <h2 className={`text-lg font-medium`}>FAQ</h2>
+        <div className={`w-[360px] h-[90px] bg-gray-400`}>
+        </div>
       </section>
-      <section className={`flex flex-col items-center`}>
-        <h2>產品</h2>
+      <section className={`flex flex-col mx-auto`}>
+        <h2 className={`text-lg font-medium`}>產品</h2>
+        <div className={`w-[360px] h-[90px] bg-gray-400`}>
+        </div>
       </section>
-      <section className={`flex flex-col items-center`}>
-        <h2>聯絡我們</h2>
+      <section className={`flex flex-col mx-auto`}>
+        <h2 className={`text-lg font-medium`}>聯絡我們</h2>
+        <div className={`w-[360px] h-[90px] bg-gray-400`}>
+        </div>
       </section>
+      <footer className={`pt-2 pb-4 text-gray-400 mx-auto text-sm border-t border-gray-400`}>
+        Copyright &copy; 2021-{(new Date()).getFullYear()} PureFunc Co., Ltd. All Rights Reserved.
+      </footer>
     </div>
   )
 }
