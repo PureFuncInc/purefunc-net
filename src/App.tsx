a8d18ffc195b454eda42f06d09c3a2fb26ef9ee5
Vincent Huang MacBook <yfr.huang@hotmail.com>
2024-01-30 20:34:46 +0800

    :art: upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import square_logo from './assets/purefunc_black.webp'
 import imac from './assets/imac.webp'
+import daniel from './assets/daniel.webp'
+import wade from './assets/wade.webp'
+import carlos from './assets/carlos.webp'
+import vincent from './assets/vincent.webp'
+import shawn from './assets/shawn.webp'
+import nelson from './assets/nelson.webp'
 import rectangle_logo from './assets/purefunc_navy.webp'
 import { SiDiscord, SiFacebook, SiGithub, SiGmail, SiLinkedin, SiYoutube } from 'react-icons/si'
 
@@ -39,7 +45,6 @@ export default function App() {
           <div className={`w-[120px] h-[160px] bg-gray-400`}>FooTinder</div>
           <div className={`w-[120px] h-[160px] bg-gray-400`}>Pheme App</div>
           <div className={`w-[120px] h-[160px] bg-gray-400`}>Han Huang Group</div>
-          <div className={`w-[120px] h-[160px] bg-gray-400`}>Blockchain Wallet Services</div>
         </div>
       </section>
       <section className={`flex flex-col mx-auto mb-8`}>
@@ -86,12 +91,54 @@ export default function App() {
       <section className={`flex flex-col mx-auto mb-8`}>
         <h2 id={`team`} className={`text-lg font-bold text-[#00386B]`}>TEAM</h2>
         <div className={`flex flex-wrap gap-2`}>
-          <div className={`w-[120px] h-[160px] bg-gray-400`}>Wade</div>
-          <div className={`w-[120px] h-[160px] bg-gray-400`}>Vincent</div>
-          <div className={`w-[120px] h-[160px] bg-gray-400`}>Nelson</div>
-          <div className={`w-[120px] h-[160px] bg-gray-400`}>Daniel</div>
-          <div className={`w-[120px] h-[160px] bg-gray-400`}>Shawn</div>
-          <div className={`w-[120px] h-[160px] bg-gray-400`}>Carlos</div>
+          <div className={`flex flex-col items-center`}>
+            <img
+              src={wade}
+              className={`w-48`}
+              alt="rectangle logo"/>
+            Wade, CEO
+            <p>#SDET</p>
+          </div>
+          <div className={`flex flex-col items-center`}>
+            <img
+              src={vincent}
+              className={`w-48`}
+              alt="rectangle logo"/>
+            Vincent
+            <p>#SRE</p>
+          </div>
+          <div className={`flex flex-col items-center`}>
+            <img
+              src={nelson}
+              className={`w-48`}
+              alt="rectangle logo"/>
+            Nelson
+            <p>#Mobile</p>
+          </div>
+          <div className={`flex flex-col items-center`}>
+            <img
+              src={daniel}
+              className={`w-48`}
+              alt="rectangle logo"/>
+            Daniel
+            <p>#Frontend</p>
+          </div>
+          <div className={`flex flex-col items-center`}>
+            <img
+              src={shawn}
+              className={`w-48`}
+              alt="rectangle logo"/>
+            Shawn
+            <p>#Backend</p>
+          </div>
+          <div className={`flex flex-col items-center`}>
+            <img
+              src={carlos}
+              className={`w-48`}
+              alt="rectangle logo"/>
+            Carlos
+            <p>#FullStack</p>
+          </div>
         </div>
       </section>
       <section className={`flex flex-col mx-auto mb-8`}>
@@ -129,8 +176,8 @@ export default function App() {
           </div>
         </div>
       </section>
-      <footer className={`pt-2 pb-4 text-gray-400 mx-auto text-xs border-t border-gray-400`}>
-      Copyright &copy; 2021-{(new Date()).getFullYear()} PureFunc Co., Ltd. All Rights Reserved.
+      <footer className={`pt-2 pb-4 w-full text-gray-400 flex flex-col items-center text-xs border-t border-gray-400`}>
+        Copyright &copy; 2021-{(new Date()).getFullYear()} PureFunc Co., Ltd. All Rights Reserved.
       </footer>
     </div>
   )
