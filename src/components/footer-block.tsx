2d487225d6b7bf14dbc1e343e7fb4fe7692472ca
Vincent Huang iMac <yfr.huang@hotmail.com>
2024-05-05 00:10:35 +0800

    :art: Refactor HTML in header and footer components
    The HTML syntax in both the header and footer components has been cleaned up to improve clarity and readability. Each link (a tag) and its corresponding icon is clearly separated onto its own line. These changes do not impact the functionality of these components.

--- src/components/footer-block.tsx
+++ src/components/footer-block.tsx
@@ -6,21 +6,32 @@ export default function FooterBlock() {
   return (
     <footer className={`mx-48 mb-8 flex flex-row items-center justify-between text-[#FFFFFF]`}>
       <div className={`flex flex-row gap-8`}>
-        <a href={`https://discord.purefunc.net`}><SiDiscord size={24}/></a>
-        <a href={`https://facebook.com/purefuncinc`}><SiFacebook size={24}/></a>
-        <a href={`https://github.com/PureFuncInc`}><SiGithub size={24}/></a>
-        <a href={`mailto:help@purefunc.net`}><SiGmail size={24}/></a>
-        <a href={`https://linkedin.com/company/purefuncinc`}><SiLinkedin size={24}/></a>
-        <a href={`https://youtube.com/@purefuncinc`}><SiYoutube size={24}/></a>
+        <a href={`https://discord.purefunc.net`}>
+          <SiDiscord size={24} />
+        </a>
+        <a href={`https://facebook.com/purefuncinc`}>
+          <SiFacebook size={24} />
+        </a>
+        <a href={`https://github.com/PureFuncInc`}>
+          <SiGithub size={24} />
+        </a>
+        <a href={`mailto:help@purefunc.net`}>
+          <SiGmail size={24} />
+        </a>
+        <a href={`https://linkedin.com/company/purefuncinc`}>
+          <SiLinkedin size={24} />
+        </a>
+        <a href={`https://youtube.com/@purefuncinc`}>
+          <SiYoutube size={24} />
+        </a>
       </div>
-      <div className={`flex flex-row`}>
+      <div className={`flex flex-row gap-2`}>
         Copyright &copy; 2021-{(new Date()).getFullYear()} PureFunc Co., Ltd. All Rights Reserved.
         <img
           src={ImageBlackLogo}
           alt={`$_purefunc Black Logo`}
           width={24}
-          height={24}
-          className={`ml-2 inline`}/>
+          height={24} />
       </div>
     </footer>
   )
--- src/components/header-block.tsx
+++ src/components/header-block.tsx
@@ -10,15 +10,27 @@ export default function HeaderBlock() {
           src={ImageNavyLogo}
           alt={`$_purefunc Navy Logo`}
           width={200}
-          height={60}/>
+          height={60} />
       </a>
       <div className={`flex flex-row items-center gap-8`}>
-        <a href={`https://discord.purefunc.net`}><SiDiscord size={24}/></a>
-        <a href={`https://facebook.com/purefuncinc`}><SiFacebook size={24}/></a>
-        <a href={`https://github.com/PureFuncInc`}><SiGithub size={24}/></a>
-        <a href={`mailto:help@purefunc.net`}><SiGmail size={24}/></a>
-        <a href={`https://linkedin.com/company/purefuncinc`}><SiLinkedin size={24}/></a>
-        <a href={`https://youtube.com/@purefuncinc`}><SiYoutube size={24}/></a>
+        <a href={`https://discord.purefunc.net`}>
+          <SiDiscord size={24} />
+        </a>
+        <a href={`https://facebook.com/purefuncinc`}>
+          <SiFacebook size={24} />
+        </a>
+        <a href={`https://github.com/PureFuncInc`}>
+          <SiGithub size={24} />
+        </a>
+        <a href={`mailto:help@purefunc.net`}>
+          <SiGmail size={24} />
+        </a>
+        <a href={`https://linkedin.com/company/purefuncinc`}>
+          <SiLinkedin size={24} />
+        </a>
+        <a href={`https://youtube.com/@purefuncinc`}>
+          <SiYoutube size={24} />
+        </a>
         <a
           href={`#contact`}
           className={`rounded-xl border-2 border-[#FFFFFF] px-4 py-2`}>
