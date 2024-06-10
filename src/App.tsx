e15796fd668ae2838a55257d8459ae9f82310183
Vincent Huang iMac <yfr.huang@hotmail.com>
2024-06-10 14:41:21 +0800

    :art: Update main.js script version in index.html
    The reference to the main JavaScript file in index.html has been updated with a new version. Also, the JavaScript source map file has been renamed to reflect the new version.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
 import Blog from './pages/blog'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <Routes>
         <Route path={`/`} element={<Home />} />
         <Route path={`/blog/*`} element={<Blog />} />
       </Routes>
-    </BrowserRouter>
+    </>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
     <React.StrictMode>
-      <App/>
+      <HashRouter>
+        <App />
+      </HashRouter>
     </React.StrictMode>
   )
