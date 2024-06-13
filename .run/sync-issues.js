9b785554ae62aaa979a4249da337b1051085ea65
Vincent Huang iMac <yfr.huang@hotmail.com>
2024-06-14 00:39:58 +0800

    :art: Add sync-issues.js and update article descriptions
    A new file named sync-issues.js has been added to automate fetching and updating issues. Additionally, the descriptions in the articles (1.md and 2.md) have been streamlined for brevity and focus. The new script will not only fetch the issues from the GitHub repo but also update the relevant article files with the issue's body content.

---
+++ .run/sync-issues.js
@@ -0,0 +1,24 @@
+const fs = require('node:fs')
+
+console.log("fetch issue started")
+
+const token = process.env.GITHUB_TOKEN
+
+let headers = {
+  'Accept': 'application/vnd.github+json',
+  'Authorization': `Bearer ${token}`,
+  'X-GitHub-Api-Version': '2022-11-28'
+}
+
+fetch('https://api.github.com/repos/PureFuncInc/purefunc-net/issues', {method: 'GET', headers: headers})
+  .then(response => response.json())
+  // .then(data => console.log(data))
+  .then(data => {
+    data.forEach(issue => {
+      fs.writeFile(`public/articles/${issue.number}.md`, issue.body, (err) => {
+        if (err) throw err
+        console.log('The file has been saved!')
+      })
+    })
+  })
+
