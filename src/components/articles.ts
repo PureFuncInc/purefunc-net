bcf0fb7c5fabda9322cd223dc5d513dc165ee36b
Vincent Huang iMac <yfr.huang@hotmail.com>
2024-06-15 21:27:18 +0800

    :art: Add new articles and update existing ones
    This commit includes the addition of two new articles, titled 'ICQ 將終止營運' and 'Google 搜尋引擎內部 API 文件外洩'. It also updates content in two existing articles, 'Cash 哥的工程師必備' and '整理了 Intellij IDEA 2020 到 2022 年底的一些更新'.

--- src/components/articles.ts
+++ src/components/articles.ts
@@ -5,11 +5,19 @@ export interface Article {
 
 export const Articles: Article[] = [
     {
+        number: 4,
+        title: 'ICQ 將終止營運',
+    },
+    {
+        number: 3,
+        title: 'Google 搜尋引擎內部 API 文件外洩',
+    },
+    {
         number: 2,
-        title: 'Microsoft is committed to the success of Java developers',
+        title: 'Cash 哥的工程師必備',
     },
     {
         number: 1,
-        title: '併發編程⚡談談鎖與資源',
+        title: '整理了 Intellij IDEA 2020 到 2022 年底的一些更新',
     },
 ]
