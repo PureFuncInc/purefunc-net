b42b9f2ba8fae4c7e70832e906fe26d70048ad60
Vincent Huang iMac <yfr.huang@hotmail.com>
2024-06-16 11:21:50 +0800

    :art: Refactor ArticlesBlock component for category listings
    The code has been updated to provide more specific areas for different categories of articles (meme, self, reference, and news). Each section of articles is now filtered and stored into its own constant array in order to declutter and optimize the render return. Furthermore, improvements have been made on the display of individual article links.

--- src/components/articles-block.tsx
+++ src/components/articles-block.tsx
@@ -3,68 +3,92 @@ import { Article, Articles } from '../components/articles'
 import { Link } from 'react-router-dom'
 
 export default function ArticlesBlock() {
+  const memes: Article[] = Articles
+    .filter((article: Article) => !article.labels.includes('draft'))
+    .filter((article: Article) => article.labels.includes('meme'))
+  const selfs: Article[] = Articles
+    .filter((article: Article) => !article.labels.includes('draft'))
+    .filter((article: Article) => article.labels.includes('self'))
+  const references: Article[] = Articles
+    .filter((article: Article) => !article.labels.includes('draft'))
+    .filter((article: Article) => article.labels.includes('reference'))
+  const newss: Article[] = Articles
+    .filter((article: Article) => !article.labels.includes('draft'))
+    .filter((article: Article) => article.labels.includes('news'))
+
   return (
     <article className={`mx-auto flex flex-col gap-4 text-xs md:text-base`}>
-      <h2>meme</h2>
+      <h1 className={`mx-auto text-3xl`}>INDEX</h1>
+      {/*<div className={`flex flex-row gap-2`}>*/}
+      {/*  <Link to={`#meme`}>meme</Link>*/}
+      {/*  <Link to={`#self`}>self</Link>*/}
+      {/*  <Link to={`#reference`}>reference</Link>*/}
+      {/*  <Link to={`#news`}>news</Link>*/}
+      {/*</div>*/}
+      <span className={`flex flex-row items-center justify-between border-b-2 border-[#000000]`}>
+        <h2 id={`meme`} className={`text-lg`}>meme</h2>
+        {memes.length}
+      </span>
       {
-        Articles
-          .filter((article: Article) => !article.labels.includes('draft'))
-          .filter((article: Article) => article.labels.includes('meme'))
+        memes
           .map((article: Article, _) => {
             return (
               <Link
                 key={`article${article.number}`}
                 to={`/blog/${article.number}`}>
                 <span className={`hidden md:mr-2 md:inline`}>{article.createdAt.substring(0, 10)}</span>
-                <span className={`border-b-2 border-[#000000]`}>{article.title}</span>
+                <span className={``}>{article.title}</span>
               </Link>
             )
           })
       }
-      <h2>self</h2>
+      <span className={`flex flex-row items-center justify-between border-b-2 border-[#000000]`}>
+        <h2 id={`self`} className={`text-lg`}>self</h2>
+        {selfs.length}
+      </span>
       {
-        Articles
-          .filter((article: Article) => !article.labels.includes('draft'))
-          .filter((article: Article) => article.labels.includes('self'))
+        selfs
           .map((article: Article, _) => {
             return (
               <Link
                 key={`article${article.number}`}
                 to={`/blog/${article.number}`}>
                 <span className={`hidden md:mr-2 md:inline`}>{article.createdAt.substring(0, 10)}</span>
-                <span className={`border-b-2 border-[#000000]`}>{article.title}</span>
+                <span className={``}>{article.title}</span>
               </Link>
             )
           })
       }
-      <h2>reference</h2>
+      <span className={`flex flex-row items-center justify-between border-b-2 border-[#000000]`}>
+        <h2 id={`reference`} className={`text-lg`}>reference</h2>
+        {references.length}
+      </span>
       {
-        Articles
-          .filter((article: Article) => !article.labels.includes('draft'))
-          .filter((article: Article) => article.labels.includes('reference'))
+        references
           .map((article: Article, _) => {
             return (
               <Link
                 key={`article${article.number}`}
                 to={`/blog/${article.number}`}>
                 <span className={`hidden md:mr-2 md:inline`}>{article.createdAt.substring(0, 10)}</span>
-                <span className={`border-b-2 border-[#000000]`}>{article.title}</span>
+                <span className={``}>{article.title}</span>
               </Link>
             )
           })
       }
-      <h2>news</h2>
+      <span className={`flex flex-row items-center justify-between border-b-2 border-[#000000]`}>
+        <h2 id={`news`} className={`text-lg`}>news</h2>
+        {newss.length}
+      </span>
       {
-        Articles
-          .filter((article: Article) => !article.labels.includes('draft'))
-          .filter((article: Article) => article.labels.includes('news'))
+        newss
           .map((article: Article, _) => {
             return (
               <Link
                 key={`article${article.number}`}
                 to={`/blog/${article.number}`}>
                 <span className={`hidden md:mr-2 md:inline`}>{article.createdAt.substring(0, 10)}</span>
-                <span className={`border-b-2 border-[#000000]`}>{article.title}</span>
+                <span className={``}>{article.title}</span>
               </Link>
             )
           })
