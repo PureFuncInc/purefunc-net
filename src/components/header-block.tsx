56f16483870f2723891a62f025605ff30f101381
Vincent Huang iMac <yfr.huang@hotmail.com>
2025-06-17 20:59:12 +0800

    chore: move GitHub workflow files from workflows directory to root .github folder

--- src/components/header-block.tsx
+++ src/components/header-block.tsx
@@ -1,28 +1,24 @@
-import ImageNavyLogo from '../assets/purefunc_navy.webp'
-import React from 'react'
-import { SiFacebook, SiGithub, SiLinkedin, SiWordpress, SiYoutube } from 'react-icons/si'
+import ImageNavyLogo from "../assets/purefunc_navy.webp";
+import React from "react";
+import { SiFacebook, SiGithub, SiLinkedin, SiWordpress, SiYoutube } from "react-icons/si";
 
 interface Props {
-  theme: string
+  theme: string;
 }
 
-export default function HeaderBlock({theme}: Props) {
-  const textColor = theme === 'light' ? 'text-[#444444]' : 'text-[#FFFFFF]'
-  const borderColor = theme === 'light' ? 'border-[#444444]' : 'border-[#FFFFFF]'
+export default function HeaderBlock({ theme }: Props) {
+  const textColor = theme === "light" ? "text-[#444444]" : "text-[#FFFFFF]";
+  const borderColor = theme === "light" ? "border-[#444444]" : "border-[#FFFFFF]";
 
   return (
     <header className={`mx-8 mt-8 flex flex-wrap items-center justify-between gap-2 ${textColor} lg:mx-24 xl:mx-36 2xl:mx-48`}>
       <a href={`/`}>
-        <img
-          src={ImageNavyLogo}
-          alt={`$_purefunc Navy Logo`}
-          width={200}
-          height={60} />
+        <img src={ImageNavyLogo} alt={`$_purefunc Navy Logo`} width={200} height={60} />
       </a>
       <div className={`flex flex-row items-center gap-8`}>
-        <a href={`/#/blog`} className={`hidden md:block`}>
+        {/* <a href={`/#/blog`} className={`hidden md:block`}>
           <SiWordpress size={24} />
-        </a>
+        </a> */}
         <a href={`https://facebook.com/purefuncinc`} className={`hidden md:block`}>
           <SiFacebook size={24} />
         </a>
@@ -35,12 +31,12 @@ export default function HeaderBlock({theme}: Props) {
         <a href={`https://youtube.com/@purefuncinc`} className={`hidden md:block`}>
           <SiYoutube size={24} />
         </a>
-        <a
+        {/* <a
           href={`#contact`}
           className={`rounded-xl border-2 ${borderColor} px-4 py-2`}>
           聯絡我們
-        </a>
+        </a> */}
       </div>
     </header>
-  )
+  );
 }
--- src/components/main-block.tsx
+++ src/components/main-block.tsx
@@ -1,21 +1,18 @@
-import ImageIMac from '../assets/imac.webp'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import ImageIMac from "../assets/imac.webp";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function MainBlock() {
   return (
     // <section className={`md:mx-16 lg:mx-24 xl:mx-36 2xl:mx-48 flex flex-wrap justify-center md:gap-16 lg:gap-24 xl:gap-36 2xl:gap-48`}>
     <section className={`mx-auto flex flex-wrap justify-center md:gap-16 lg:gap-24 xl:gap-36 2xl:gap-48`}>
       <nav className={`hidden justify-center gap-8 text-xl font-medium text-[#FFFFFF] md:flex md:flex-col`}>
-        <Link to={`/blog`}>技術網誌</Link>
+        {/* <Link to={`/blog`}>技術網誌</Link> */}
         {/*<a href={`#services`}>服務類型</a>*/}
         {/*<a href={`#product`}>產品專案</a>*/}
         <Link to={`#team`}>團隊成員</Link>
       </nav>
-      <img
-        src={ImageIMac}
-        className={`w-[360px] md:w-[425px] lg:w-[625px] xl:w-[725px] 2xl:w-[800px]`}
-        alt={`write an email to $_purefunc`}/>
+      <img src={ImageIMac} className={`w-[360px] md:w-[425px] lg:w-[625px] xl:w-[725px] 2xl:w-[800px]`} alt={`write an email to $_purefunc`} />
     </section>
-  )
+  );
 }
